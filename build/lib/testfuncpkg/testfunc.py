c9e2f5d08ba03ae0e8e37b45f1bfb4389a632871
td2014 <anthonyd2004@gmail.com>
2019-03-16 15:39:30 -0700

    added wheel

---
+++ build/lib/testfuncpkg/testfunc.py
@@ -0,0 +1,11 @@
+#
+# Test function for pip install in Python 3.X environments
+#
+def testfunc_one():
+     print('Running testfunc_one: Python 3.X function for pip install testing.')
+
+     return 'Python 3.X test function result'
+
+#
+# End of function
+#
